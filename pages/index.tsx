99a7c25055a97c437b04fd56480f3b48198f4d95
dodeca-dev <jrussclay@gmail.com>
2021-02-09 18:52:12 +0800

    added initial setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/core/layout/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => <Layout title="Home">home page</Layout>;
 
-export default IndexPage
+export default IndexPage;
